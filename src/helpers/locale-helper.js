145ad1e3bfea4e7f84ce251ac9059c90e9fc42d3
Yigit Er <mr.yigiter222@gmail.com>
2023-10-08 23:39:36 +0300

    v1

--- src/helpers/locale-helper.js
+++ src/helpers/locale-helper.js
@@ -1,30 +1,27 @@
-import { useSelector } from "react-redux"
-import store from "../store"
+import { useSelector } from "react-redux";
+import store from "../store";
 import { setLang } from "../store/slices/locale-slices";
 
 // const lang = useSelector(state=>state.locale.lang)
 // usestate kullanmak yerine store u import edip oradan state ulaşada biliriz çünkü component olmadan useSelector hookunu kullanamıyoruz
 
 export const langs = [
-    {title:"English",code:"en",country:"gb"},
-    {title:"Turkish",code:"tr",country:"tr"},
-    {title:"Franch",code:"fr",country:"fr"},
-
-]
+	{ title: "English", code: "en", country: "gb" },
+	{ title: "Turkish", code: "tr", country: "tr" },
+	{ title: "Franch", code: "fr", country: "fr" },
+];
 // state ulaşmak için
-export const getCurrentLang = () => { 
-   return store.getState().locale.lang;
-}
+export const getCurrentLang = () => {
+	return store.getState().locale.lang;
+};
 // state i güncellemek için
-export const setCurrentLang = (lang) => { 
-    store.dispatch(setLang(lang))
-
-}
+export const setCurrentLang = (lang) => {
+	store.dispatch(setLang(lang));
+};
 
 //translate function
-export const $t =(key) =>{
-const currentLang = getCurrentLang();
-const langFile = require(`../helpers/langs/${currentLang.code}.json`)
-return langFile[key];
-}
-
+export const $t = (key) => {
+	const currentLang = getCurrentLang();
+	const langFile = require(`../helpers/langs/${currentLang.code}.json`);
+	return langFile[key];
+};
