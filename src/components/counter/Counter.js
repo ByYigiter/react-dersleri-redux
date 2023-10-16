45c01d4b987c071b5620aa1adca90686cdc7cb48
Yigit Er <mr.yigiter222@gmail.com>
2023-10-16 22:53:39 +0300

    v2

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import {decrement,increment} from "../../store/slices/counter-slice"
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../../store/slices/counter-slice";
 
 const Counter = () => {
-    const count = useSelector((state)=>state.counter.value);
-    const dispatch = useDispatch();
-  return (
-    <div style={{display:"flex" ,gap:"10px"}}>
-        <Button onClick={()=>dispatch(decrement())}>-</Button>
-        <span>{count}</span>
-        <Button  onClick={()=>dispatch(increment())}>+</Button>
-    </div>
-  )
-}
+	const count = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+	return (
+		<div style={{ display: "flex", gap: "10px" }}>
+			<Button onClick={() => dispatch(decrement())}>-</Button>
+			<span>{count}</span>
+			<Button onClick={() => dispatch(increment())}>+</Button>
+		</div>
+	);
+};
 
-export default Counter
+export default Counter;
--- src/components/dark-mode/dark-mode-switcher.js
+++ src/components/dark-mode/dark-mode-switcher.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import { setDarkMode } from '../../store/slices/theme-slice'
-import { $t } from '../../helpers/locale-helper'
+import React from "react";
+import { Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { setDarkMode } from "../../store/slices/theme-slice";
+import { $t } from "../../helpers/locale-helper";
 
 const DarkModeSwitcher = () => {
-    const darkMode = useSelector((state)=>state.theme.darkMode);  // dark modeu merkezi stateden almak için kullanılır
-    const dispatch = useDispatch();// değiştirmek için kullanılır
-  return (
-    <div>
-        <Form.Check
-        className='m-5'
-        type="switch"
-        id="dmSwitch"
-        label={darkMode ? $t("dark-mode") :$t("light-mode")}
-        checked={darkMode}
-        onChange={()=>dispatch(setDarkMode(!darkMode))}
-        />
-    </div>
-  );
-}
+	const darkMode = useSelector((state) => state.theme.darkMode); // dark modeu merkezi stateden almak için kullanılır
+	const dispatch = useDispatch(); // değiştirmek için kullanılır
+	return (
+		<div>
+			<Form.Check
+				className="m-5"
+				type="switch"
+				id="dmSwitch"
+				label={darkMode ? $t("dark-mode") : $t("light-mode")}
+				checked={darkMode}
+				onChange={() => dispatch(setDarkMode(!darkMode))}
+			/>
+		</div>
+	);
+};
 export default DarkModeSwitcher;
