877cef73b4164a6ad75d8d5ba7391f93a30c8aeb
Yigit Er <mr.yigiter222@gmail.com>
2023-10-17 23:21:34 +0300

    v1

--- src/store/slices/theme-slice.js
+++ src/store/slices/theme-slice.js
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const themeSlice = createSlice({
-    //create slice  bir obje gönderiyoruz bu obje 3 key den oluşur name,initialState ve reducers
-    name:"theme",
-    initialState:{
-        darkMode:false
-    },
-    reducers:{
-        setDarkMode:(state,action)=>{  //paremetre olarak state ve  gerekirse action veriyoruz
-            state.darkMode = action.payload;  // actiondan gelen yani setterdan kullanıcıdan gelen değeri merkezi state deki değişkene aktar
-        }
-
-    }
-})
-export const {setDarkMode} = themeSlice.actions;
+	//create slice  bir obje gönderiyoruz bu obje 3 key den oluşur name,initialState ve reducers
+	name: "theme",
+	initialState: {
+		darkMode: false,
+	},
+	reducers: {
+		setDarkMode: (state, action) => {
+			//paremetre olarak state ve  gerekirse action veriyoruz
+			state.darkMode = action.payload; // actiondan gelen yani setterdan kullanıcıdan gelen değeri merkezi state deki değişkene aktar
+		},
+	},
+});
+export const { setDarkMode } = themeSlice.actions;
 export default themeSlice.reducer;
