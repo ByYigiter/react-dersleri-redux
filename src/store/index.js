28807ac064f4b4b07bb5160119f51460caa6c711
Yigit Er <mr.yigiter222@gmail.com>
2023-10-17 08:11:58 +0300

    v1

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,14 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./slices/theme-slice"; // buradaki import theme-slice sayfasındaki 17 satır yani defaultu import etti {} olsaydı 16 satır ı kullanırdı***
-import localSlice  from "./slices/locale-slices";
+import localSlice from "./slices/locale-slices";
 import counterSlice from "./slices/counter-slice";
 
 export default configureStore({
-    // slices içinde oluşturaun here slice için reducer objesine ekleme yapıyoruz configure ediyoruz
-    reducer:{
-        theme:themeSlice,
-        locale:localSlice,
-        counter:counterSlice
-    }
-})
+	// slices içinde oluşturaun here slice için reducer objesine ekleme yapıyoruz configure ediyoruz
+	reducer: {
+		theme: themeSlice,
+		locale: localSlice,
+		counter: counterSlice,
+	},
+});
